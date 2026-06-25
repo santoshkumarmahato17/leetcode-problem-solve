a762e25154092804e597f5593874da290e332104
Santosh Kumar Mahato <kr.santoshmahato@gmail.com>
2026-06-25 19:49:36 +0530

    Time: 0 ms (100%), Space: 11.8 MB (11.3%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+void swap(int *a, int *b)
+    {
+        int temp = *a;
+        *a = *b;
+        *b = temp;
+    }
+    void sortColors(vector<int>& nums) {
+        for (int i = 0; i < nums.size() - 1; i++)
+        {
+            for (int j = 0; j < nums.size() - i - 1; j++)
+            {
+                if (nums[j] > nums[j + 1])
+                {
+                    swap(&nums[j], &nums[j + 1]);
+                }
+            }
+        }
+    }
+};
